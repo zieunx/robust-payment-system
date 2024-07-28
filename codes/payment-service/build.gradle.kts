b65356b64f26dd62f78f4e0a1a689fd65a35f482
zieunx <wldms_1217@naver.com>
2024-07-28 21:10:10 +0900

    feat: 결제승인메시지발행 (Transactional Outbox Pattern, Apache Kafka)

--- codes/payment-service/build.gradle.kts
+++ codes/payment-service/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.impldep.org.junit.platform.launcher.TagFilter.excludeTags
+
 plugins {
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
@@ -22,10 +24,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    implementation("org.springframework.cloud:spring-cloud-stream")
+    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka-reactive")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
+
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("io.asyncer:r2dbc-mysql")
 
@@ -36,6 +45,14 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+extra["springCloudVersion"] = "2023.0.0"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
 kotlin {
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
@@ -44,4 +61,5 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+//    excludeTags("ExternalIntegration")
 }
--- codes/payment-service/src/main/kotlin/org/example/paymentservice/PaymentServiceApplication.kt
+++ codes/payment-service/src/main/kotlin/org/example/paymentservice/PaymentServiceApplication.kt
@@ -2,11 +2,27 @@ package org.example.paymentservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.messaging.Message
 import org.springframework.scheduling.annotation.EnableScheduling
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.util.function.Function as Function
 
 @EnableScheduling
 @SpringBootApplication
-class PaymentServiceApplication
+class PaymentServiceApplication{
+
+    @Bean
+    fun consume(): Function<Flux<Message<String>>,  Mono<Void>> {
+        return Function { messages ->
+            messages.map {
+                println(it)
+                it
+            }.then()
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<PaymentServiceApplication>(*args)
